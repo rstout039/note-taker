90c3a99958d87db03a5369224dccad171477ec3e
Ryan Stout <rstout039@gmail.com>
2020-12-01 19:12:13 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-// Require Dependencies
+// Dependencies
 const express = require("express");
-const fs = require("fs");
-const path = require('path');
+const apiRoutes = require("./routes/apiRoutes.js");
+const htmlRoutes = require("./routes/htmlRoutes.js");
 
-// Initialize express app
+// Create an express server
 const app = express();
+
+// Create a port
 const PORT = process.env.PORT || 3000;
 
-// Setup data parsing
-app.use(express.urlencoded({ extended: true }));
+// Sets up the Express app to handle data parsing
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.urlencoded({ extended: true }));
 
-//Require routes file
-require('./routes/routes')(app);
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-// Setup listener
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-});  
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
